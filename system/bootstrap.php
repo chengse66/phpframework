0182326d610dd63099af6b8499165c3f20594897
橙色下午茶 <nease@vip.qq.com>
2017-03-01 09:39:03 +0800

    修复了个别情况不执行的情况

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -86,11 +86,11 @@ class bootstrap{
      */
     private static function __run() {
         $URL=$_SERVER["REQUEST_URI"];
-        $p=strpos($URL, "?");
         if(self::path_rel()!=""){
             $rel=str_replace("/", "\\/", self::path_rel());
             $URL=preg_replace("/^\\/$rel/", "", $URL);
         }
+        $p=strpos($URL, "?");
         if($p!=false && $p>=0) $URL=substr($URL, 0,$p);
         $URL='/'.preg_replace("/(^\\/)|(\\/$)/", "", $URL);
         ksort(self::$_map,SORT_STRING);
