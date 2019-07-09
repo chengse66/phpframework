7b24b6878f33639fe3c421e5cb1a26b4eafc2862
橙色下午茶 <nease@vip.qq.com>
2019-07-09 14:30:57 +0800

    #2019070902

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -227,8 +227,9 @@ class bootstrap{
     		$url.=(strpos($url, "?")?"&":"?").$query;
     	}
     	curl_setopt($ch, CURLOPT_URL, $url);
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    	curl_setopt($ch, CURLOPT_HEADER, 0);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     	curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
     	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
     	$result = curl_exec($ch);
