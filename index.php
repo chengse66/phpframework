ade4123bb822c3d633357e30fb8c213e879719a6
橙色下午茶 <nease@vip.qq.com>
2022-07-28 16:37:56 +0800

    #20220728

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php
-define("DEBUG",true);
-require 'system/bootstrap.php';
+define("DEBUG", true);
+require './../system/bootstrap.php';
 bootstrap::start("app");
-ww_route("Hello", "test");
+$do=$_REQUEST["do"];
+if(empty($do)) $do="home";
+if(!empty($do) && bootstrap::controller("test", $do)!==FALSE){
+    ww_route("test", $do);
+}else{
+    ww_route("test", "home");
+}
